79b02e75ddf98ef85935373555a8f267383e02fc
Jampool G <jampoolgarcia@gmail.com>
2023-04-28 06:53:31 -0400

    added rauting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: "home", pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '**', redirectTo: "home", pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
