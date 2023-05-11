47f57f2f4d15c8d12752666f9e7a0623e1b4a701
Jampool G <jampoolgarcia@gmail.com>
2023-05-11 06:41:16 -0400

    added user details template

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -12,10 +12,12 @@ export class UserDetailsComponent implements OnInit {
 
   public id!: string;
   public user!: UserI;
+  public imgUrl = 'https://phantom-marca.unidadeditorial.es/4c7e775d1e2a58f3c271fb52048e4262/resize/1320/f/jpg/assets/multimedia/imagenes/2022/10/10/16654222870042.jpg';
+  
 
   constructor(private aRoute: ActivatedRoute, private _service: UserService) {
     this.id = this.aRoute.snapshot.paramMap.get('id')!;
-   }
+  }
 
   ngOnInit(): void {
     this.getUser();
@@ -25,6 +27,7 @@ export class UserDetailsComponent implements OnInit {
     this._service.getOneUser(this.id).subscribe(
       res => {
         console.log(res);
+        this.user = res;
       }
     )
   }
